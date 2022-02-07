cc9d1a53435d5beffcd0ccda0754a579f51e8c6c
IlyaNikolaenko <illya7272.51@gmail.com>
2022-02-07 22:43:02 +0200

    Classwork 28

--- src/pages/movie/index.js
+++ src/pages/movie/index.js
@@ -10,18 +10,31 @@ export const MoviePage = () => {
     const movies = useSelector((state) => state.movies);
 
     const movie = useMemo(() => movies.find((item) => item.id === +id), [id, movies])
-
+    console.log(movie)
     return(
         <Spinner>
             <div className="movie-page">
                 <h1>{movie.title}</h1>
                 <div className="movie-info">
                     <div>
-                        <img src={movie.url} alt="poster"/>
+                        <img src={movie.poster} alt="poster"/>
                         <div className="btn-buy">Buy ticket</div>
                     </div>
                     <div>
-                        <Info label="Actors" value={movie.actors.join(", ")}/>
+                        <Info label="Actors" value={movie.actors.join(', ')}/>
+                        <Info label="Genres" value={movie.genre.join(', ')}/>
+                        <Info label="Countries" value={movie.country.join(', ')}/>
+                        <Info label="Language" value={movie.language}/>
+                        <Info label="Age" value={movie.age ? `${movie.age}+`: "No age restrictions"}/>
+                        <Info label="Description" value={movie.description}/>
+                        <Info label="Trailer" value={
+                            <iframe
+                                title="video"
+                                width="100%"
+                                height="400px"
+                                src={movie.trailer}
+                                allowFullScreen
+                            />} />
                     </div>
                 </div>
             </div>
